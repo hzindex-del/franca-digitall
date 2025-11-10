3989305b108b54f9bc7478796118d56af3526601
hzindex-del <hzinn603@gmail.com>
2025-11-10 16:41:32 -0300

    js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const texto = document.getElementById('texto').innerText;
 console.log(texto)
 alert("mauro ou oruam?" + texto)
+
+const campoNome = document.getElementById("input")
+campoNome.addEventListener("input", (e) => {
+    console.log(e.target.value)
+})
+
+const caixa = document.getElementById
